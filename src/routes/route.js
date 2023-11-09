b1fc9bae2b479eab6e2008bc64e55e233ca955f9
Rafael <francorfs@icloud.com>
2023-11-09 01:01:12 -0300

    endpoint listar categorias inserido

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const { registerUser } = require('../controllers/users')
+const { listCategory } = require('../controllers/category')
 
 const rotas = express()
 
 rotas.get('/usuarios', registerUser)
+rotas.get('/categorias', listCategory)
 
 module.exports = rotas
