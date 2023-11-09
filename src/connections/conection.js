70f61f1e903a0bd55c5b9faead02d116dda8be2d
Rafael <francorfs@icloud.com>
2023-11-09 00:52:30 -0300

    BD criado no site render.com

--- src/connections/conection.js
+++ src/connections/conection.js
@@ -2,10 +2,10 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
         user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASS,
         port: process.env.DB_PORT,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATA,
+        password: process.env.DB_PASS,
     }
 })
 
