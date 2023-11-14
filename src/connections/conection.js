d8649a987dbe35ae68c1c0717d9c08b2565c30c1
Rafael <francorfs@icloud.com>
2023-11-14 16:43:18 -0300

    Conexao BD com render

--- src/connections/conection.js
+++ src/connections/conection.js
@@ -1,12 +1,20 @@
-const knex = require('knex')({
-    client: 'pg',
-    connection: {
-        user: process.env.DB_USER,
-        port: process.env.DB_PORT,
-        host: process.env.DB_HOST,
-        database: process.env.DB_DATA,
-        password: process.env.DB_PASS,
-    }
-})
+// const knex = require('knex')({
+//     client: 'pg',
+//     connection: {
+//         user: process.env.DB_USER,
+//         port: process.env.DB_PORT,
+//         host: process.env.DB_HOST,
+//         database: process.env.DB_DATA,
+//         password: process.env.DB_PASS,
+//     }
+// })
 
-module.exports = knex
+// module.exports = knex
+
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require"
+})
+module.exports = pool;
